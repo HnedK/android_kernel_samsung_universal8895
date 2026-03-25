44da8b534fa305fc6d4faf09c733bb6190a749ae
HnedK <keandrewjl@gmail.com>
2026-03-25 14:42:42 +0900

    arm64: ion: Fix empty ion.h headers breaking videobuf2-ion
    include/linux/ion.h and include/uapi/linux/ion.h were accidentally
    committed as 0-byte files in the initial clone, preventing dependent
    drivers like videobuf2-ion from resolving ION_NUM_HEAPS and
    ION_HEAP_SYSTEM_MASK. Replaced them with cross-includes to the actual
    headers in drivers/staging/android/.

---
+++ include/linux/ion.h
@@ -0,0 +1,4 @@
+#ifndef _LINUX_ION_H_WRAPPER
+#define _LINUX_ION_H_WRAPPER
+#include "../../drivers/staging/android/ion/ion.h"
+#endif
---
+++ include/uapi/linux/ion.h
@@ -0,0 +1,4 @@
+#ifndef _UAPI_LINUX_ION_H_WRAPPER
+#define _UAPI_LINUX_ION_H_WRAPPER
+#include "../../../drivers/staging/android/uapi/ion.h"
+#endif
